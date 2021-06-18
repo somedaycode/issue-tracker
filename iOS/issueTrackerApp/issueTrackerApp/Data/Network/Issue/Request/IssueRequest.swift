dc60077594eaecb9897a0bb6d2357ba1add7a0cb
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2021-06-18 17:35:21 +0900

    Fix: #46 더미이슈데이터 추가

--- iOS/issueTrackerApp/issueTrackerApp/Data/Network/Issue/Request/IssueRequest.swift
+++ iOS/issueTrackerApp/issueTrackerApp/Data/Network/Issue/Request/IssueRequest.swift
@@ -20,7 +20,7 @@ extension IssueRequest: JSONDataRequest {
     
     var fetchReq: URLRequest {
         let urlComponents = URLComponents(url: IssueEndPoint.url(), resolvingAgainstBaseURL: false)!
-        var request = try! URLRequest(url: urlComponents, method: .get)
+        let request = try! URLRequest(url: urlComponents, method: .get)
 //        request.setValue("Authorization", forHTTPHeaderField: "Barear eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdmF0YXJfdXJsIjoiaHR0cHM6Ly9hdmF0YXJzLmdpdGh1YnVzZXJjb250ZW50LmNvbS91LzE2Njk0MzQ2P3Y9NCIsIm5hbWUiOiJNSmJhZSIsImlzcyI6Imlzc3VlLXRyYWNrZXIiLCJpZCI6MTY2OTQzNDZ9.fuyAnE21exNCg5piU3p-_-U67jqoQEUPtClfLCxkElU")
         return request
     }
