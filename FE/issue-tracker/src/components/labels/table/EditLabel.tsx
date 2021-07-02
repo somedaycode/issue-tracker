c9ae770a37e017d5820bac9e27be2f1c56fbd324
eve712 <62237639+eve712@users.noreply.github.com>
2021-07-02 23:07:49 +0900

    Feat: #83 라벨 편집 시 Input에 라벨 정보 전달

--- FE/issue-tracker/src/components/labels/table/EditLabel.tsx
+++ FE/issue-tracker/src/components/labels/table/EditLabel.tsx
@@ -1,12 +1,18 @@
 import styled from 'styled-components';
+
 import LabelInputBox from '@components/labels/LabelInputBox';
 import EditCompleteBtn from '@components/common/EditCompleteBtn';
+import { labelInfoType } from './LabelCell';
 
-function EditLabel() {
+type Props = {
+  labelInfo: labelInfoType;
+};
+
+function EditLabel({ labelInfo }: Props) {
   return (
     <EditLabelWrap>
       <h2>레이블 편집</h2>
-      <LabelInputBox>
+      <LabelInputBox labelInfo={labelInfo}>
         <EditCompleteBtn />
       </LabelInputBox>
     </EditLabelWrap>
