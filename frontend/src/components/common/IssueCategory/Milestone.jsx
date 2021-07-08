338ce8e608bf7d50f0b6a0627d1c8315f274662f
Dami Kim <dami.k.dev@gmail.com>
2021-07-08 17:58:10 +0900

    feat: 이슈 카테고리 milestone progress bar 추가(#112)

--- frontend/src/components/common/IssueCategory/Milestone.jsx
+++ frontend/src/components/common/IssueCategory/Milestone.jsx
@@ -1,28 +1,19 @@
 import styled from "styled-components";
 import { milestoneCategoryState } from "RecoilStore/Atoms";
 import { useRecoilValue } from "recoil";
+import getPercent from "util/getPercent";
 const Milestone = () => {
 	const milestoneData = useRecoilValue(milestoneCategoryState);
+	const { closedIssues, openIssues } = milestoneData;
 	return (
 		<>
-			<MilestoneTotalProgressBar />
+			<ProgressBar value={closedIssues} max={closedIssues + openIssues} />
 			{milestoneData && <ContentsText>{milestoneData.title}</ContentsText>}
 		</>
 	);
 };
-
-const MilestoneTotalProgressBar = styled.div`
+const ProgressBar = styled.progress`
 	width: 100%;
-	height: 8px;
-	border-radius: 10px;
-	//이부분 진행률로 수정 바랍니다~
-	background: linear-gradient(
-		90deg,
-		#007aff 0%,
-		#007aff 50.11%,
-		#f7f7fc 50.12%,
-		#f7f7fc 100%
-	);
 `;
 
 const ContentsText = styled.div`
