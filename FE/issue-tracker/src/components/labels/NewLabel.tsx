a7a2c77052565eb17e4c298b587f30b43b1469cc
eve712 <62237639+eve712@users.noreply.github.com>
2021-07-21 16:35:17 +0900

    Fix: #81 labelInfo를 defaultLabel로 객체명 변경

--- FE/issue-tracker/src/components/labels/NewLabel.tsx
+++ FE/issue-tracker/src/components/labels/NewLabel.tsx
@@ -3,7 +3,7 @@ import LabelInputBox from '@components/labels/LabelInputBox';
 import CompleteBtn from '@components/common/CompleteBtn';
 
 function NewLabel() {
-  const labelInfo = {
+  const defaultLabel = {
     title: '레이블 이름',
     description: '',
     color_code: '#cee1e2',
@@ -13,7 +13,7 @@ function NewLabel() {
   return (
     <NewLabelWrap>
       <h2>새로운 레이블 추가</h2>
-      <LabelInputBox labelInfo={labelInfo}>
+      <LabelInputBox labelInfo={defaultLabel}>
         <CompleteBtn />
       </LabelInputBox>
     </NewLabelWrap>
