21e85c43815f315a84a00c998dfd43a05b4c47ed
eve712 <62237639+eve712@users.noreply.github.com>
2021-07-25 01:32:36 +0900

    Feat: #81 useReducer에서 recoil로 상태관리 변경
    * useReducer가 가독성이나 방식이 더 뛰어난 편이 아니라 굳이 사용할 이유가 없음
    * 완료 버튼을 children으로 받아 상태값들을 부모가 알아야 하는 구조라 전역 상태인 recoil로 관리

---
+++ FE/issue-tracker/src/store/atoms/labelInputs.ts
@@ -0,0 +1,16 @@
+import { atom } from 'recoil';
+
+export const titleValueAtom = atom({
+  key: 'titleValueAtom',
+  default: '',
+});
+
+export const descriptionValueAtom = atom({
+  key: 'descriptionValueAtom',
+  default: '',
+});
+
+export const colorValueAtom = atom({
+  key: 'colorValueAtom',
+  default: '',
+});
